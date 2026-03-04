d81b681c8e8e202e965123af2b5caa989ad6d433
znaidemi-coder <znaidemi@gmail.com>
2026-03-04 12:12:27 +0100

    Delete netlify/functions/submit-contact.js

--- netlify/functions/submit-contact.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ ok: true, message: "Function is live ✅" }),
-  };
-};
